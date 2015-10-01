e6c2906ea9ef32bb4fa2e66cfbd66f4a8f956a0f
charann <charan.ky192@gmail.com>
2015-10-01 00:44:34 -0400

    Update pdpController.js

--- app/components/product/pdp/pdpController.js
+++ app/components/product/pdp/pdpController.js
@@ -1,10 +1,26 @@
 module.exports = [
-    '$scope', 'ProductService',
-    function($scope, ProductService) {
-        $scope.product = ProductService.get();
-        console.log($scope.product);
-        $scope.updateProduct = function(newName) {
-            ProductService.switch(newName);
-        };
-    }
-]
+  '$scope', 'ProductService',
+  function($scope, ProductService) {
+
+    $scope.updateProduct = function(id, product) {
+      method = product !== 'undefined' ? 'getBySku' : 'getByProdId';
+
+      ProductService[method](id)
+        .then(
+          function success(res) {
+            console.log(res);
+            $scope.product = res;
+            $scope.$broadcast('ProductUpdated');
+            console.log('Product Updated');
+          },
+          // @TODO: Handle Error
+          function failure(err) {
+            console.log(err);
+            console.log('Danger Will Robinson');
+          }
+        );
+    };
+
+    $scope.updateProduct(123456); // SKU
+  }
+];
