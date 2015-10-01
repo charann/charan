3e3f308a1ee47460e164d6327fc9950fd2fab39a
charann <charan.ky192@gmail.com>
2015-10-01 00:46:43 -0400

    Update skuRefinementDropdownDirective.js

--- app/components/product/sku/dropdown/skuRefinementDropdownDirective.js
+++ app/components/product/sku/dropdown/skuRefinementDropdownDirective.js
@@ -1,12 +1,10 @@
 module.exports = function() {
-    return {
-        transclude: true,
-        restrict: 'E',
-        templateUrl: '/app/components/product/sku/dropdown/skuRefinementDropdownTemplate.html',
-        scope: {
-            parentindex: '=',
-            product: '='
-        },
-        controller: require('./skuRefinementDropdownController')
-    };
+  return {
+    transclude: true,
+    restrict: 'E',
+    templateUrl: '/app/components/product/sku/dropdown/skuRefinementDropdownTemplate.html',
+    scope: {parentindex: '=',
+            product: '='},
+    controller: require('./skuRefinementDropdownController')
+  };
 };
