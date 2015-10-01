3b0d46b6203e74dcd4c25778f14dbaa0d53f9dad
charann <charan.ky192@gmail.com>
2015-10-01 00:38:51 -0400

    Update productDetailsStationaryDirective.js

--- app/components/product/menu/stationary/productDetailsStationaryDirective.js
+++ app/components/product/menu/stationary/productDetailsStationaryDirective.js
@@ -1,9 +1,8 @@
-module.exports = function() {
-    return {
-        restrict: 'E',
-        transclude: true,
-        replace: true,
-        templateUrl: '/app/components/product/menu/stationary/productDetailsStationaryTab.html',
-        controller: require('./productDetailsStationaryController')
-    };
+module.exports = function (){
+	return{
+		restrict: 'E',
+		transclude: true,
+		templateUrl: '/app/components/product/menu/stationary/productDetailsStationaryTab.html',
+		 controller: require('./productDetailsStationaryController')
+	};
 };
