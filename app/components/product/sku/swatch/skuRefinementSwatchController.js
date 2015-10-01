fb3588483eea042f10b8ad4641067a799734ffeb
charann <charan.ky192@gmail.com>
2015-10-01 00:54:18 -0400

    Update skuRefinementSwatchController.js

--- app/components/product/sku/swatch/skuRefinementSwatchController.js
+++ app/components/product/sku/swatch/skuRefinementSwatchController.js
@@ -1,6 +1,35 @@
 module.exports = [
-    '$scope', '$attrs',
-    function($scope, $attrs) {
-        $scope.parentIndex = $attrs.parentindex;
+  '$scope', '$attrs',
+  function($scope, $attrs) {
+    $scope.activeSku = $scope.product.details.skuId;
+    $scope.parentIndex = $attrs.parentindex;
+
+    for(i=0;i<$scope.product.skuRefinements.skuGroups[$scope.parentIndex].contents.length;i++) {
+      if($scope.product.skuRefinements.skuGroups[$scope.parentIndex].contents[i].skuId == $scope.product.details.skuId)
+      {
+        $scope.defaultOffset = i;
+        $scope.defaultTitle = $scope.product.skuRefinements.skuGroups[$scope.parentIndex].contents[i].label;
+        break;
+      }
     }
+
+    $scope.title = $scope.defaultTitle;
+
+    // Get default title
+
+    $scope.setActiveSku = function(activeSku, title)
+    {
+      if($scope.product.skuRefinements.validSkus.indexOf(activeSku) > -1) {
+        $scope.activeSku = activeSku;
+        $scope.title = title;
+      }
+      else
+      {
+        $scope.activeSku = $scope.product.details.skuId;
+        $scope.title=$scope.defaultTitle;
+      }
+    };
+
+
+  }
 ];
