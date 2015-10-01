fcd80f4e453a414b73b9316f3ae0ca7a4732b225
charann <charan.ky192@gmail.com>
2015-10-01 00:59:39 -0400

    Update skuRefinementThumbnailController.js

--- app/components/product/sku/thumbnail/skuRefinementThumbnailController.js
+++ app/components/product/sku/thumbnail/skuRefinementThumbnailController.js
@@ -1,6 +1,45 @@
 module.exports = [
-    '$scope', '$attrs',
-    function($scope, $attrs) {
-        $scope.parentIndex = $attrs.parentindex;
+  '$scope', '$attrs',
+  function($scope, $attrs) {
+    /*
+      Initialize
+    */
+
+    $scope.activeSku = $scope.product.details.skuId;
+    $scope.parentIndex = $attrs.parentindex;
+
+    for(i=0;i<$scope.product.skuRefinements.skuGroups[$scope.parentIndex].contents.length;i++) {
+      if($scope.product.skuRefinements.skuGroups[$scope.parentIndex].contents[i].skuId == $scope.product.details.skuId)
+      {
+        $scope.defaultOffset = i;
+        $scope.defaultTitle = $scope.product.skuRefinements.skuGroups[$scope.parentIndex].contents[i].label;
+        break;
+      }
     }
+
+    $scope.title = $scope.defaultTitle;
+
+    /*
+      Update Active SKU.
+
+      This will also be passing data to parent scope for updated hero image
+    */
+
+    $scope.setActiveSku = function(activeSku, title)
+    {
+      // If the SKU is valid / active
+      if($scope.product.skuRefinements.validSkus.indexOf(activeSku) > -1) {
+        $scope.activeSku = activeSku;
+        $scope.title = title;
+      }
+      else
+      {
+        // Otherwise use default
+        $scope.activeSku = $scope.product.details.skuId;
+        $scope.title=$scope.defaultTitle;
+      }
+    };
+
+
+  }
 ];
